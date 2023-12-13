43e48cff33a43d4c327efd616588ccf3f81e04e9
qinbinbin <qinbinbin@360.cn>
2023-12-13 20:12:55 +0800

    feat(extra): 网页内容处理新增文本过滤
    1. 新增文本过滤函数，只保留汉字、字母、数字和标点
    2. 恢复原始文本的原样，用文本过滤函数来调整所需内容

--- extra/website_text.py
+++ extra/website_text.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 # @Time    : 2023/12/13 14:31
 # @File    : website_text.py
+import re
+
 import requests
 from bs4 import BeautifulSoup
 
@@ -20,14 +22,22 @@ def get_all_text(url):
 
         # 打印或返回所有文本
         # print(all_text)
-        return all_text.replace("\n", "").replace(" ", "")
+        return all_text
     else:
         print(f"请求失败，状态码: {response.status_code}")
         return None
+
+
+def clear_character(sentence):
+    """ 只保留汉字、字母、数字 """
+    pattern = re.compile('[^，。！？.:：\u4e00-\u9fa5^a-zA-Z^0-9]')
+    line = re.sub(pattern, '', sentence.lower())
+    new_sentence = ''.join(line.split())  # 去除空白
+    return new_sentence
 
 
 if __name__ == '__main__':
     # 用实际的网页URL调用函数
     url = 'https://mp.weixin.qq.com/s/6KUUuQ9M3pI-v6QoRX02WA'
     res = get_all_text(url)
-    print(res)
+    print(clear_character(res))
