d0d90008b4af3c25dc7960ee4f7d01013b501adc
qinbinbin <qinbinbin@360.cn>
2023-12-18 16:30:31 +0800

    feat(extra): 扩展新增测试函数
    1. 新增测试函数用于测试url获取的全量txt，并去除无用字符
    2. 新增测试函数，用于调用llm来将文本转换为想要的格式

--- extra/website_text.py
+++ extra/website_text.py
@@ -7,6 +7,8 @@ import re
 import requests
 from bs4 import BeautifulSoup
 
+from extra.llm import get_chat_completions_data
+
 
 def get_all_text(url):
     # 发送HTTP请求获取网页内容
@@ -36,8 +38,30 @@ def clear_character(sentence):
     return new_sentence
 
 
+def test_get_all_text(url):
+    res = get_all_text(url)
+    return clear_character(res)
+
+
+def test_llm(url):
+    res = get_all_text(url)
+    content = clear_character(res)
+
+    if content:
+        content = "下面文字中，准确给出展览的开始时间（start_time）、结束时间（end_time）、地点(location)、票价（price）和名称(name)，并以json形式返回，若某个字段未提及，则返回空值期望格式[{{}}]。\n {}".format(
+            content)
+        message = [
+            {"role": "user", "content": content},
+        ]
+        return get_chat_completions_data(messages=message)
+
+
 if __name__ == '__main__':
     # 用实际的网页URL调用函数
     url = 'https://mp.weixin.qq.com/s/6KUUuQ9M3pI-v6QoRX02WA'
-    res = get_all_text(url)
-    print(clear_character(res))
+    print("========================get_all_text=========================")
+    website_text = test_get_all_text(url)
+    print(website_text)
+    print("========================llm=========================")
+    show_list = test_llm(url)
+    print(show_list)
